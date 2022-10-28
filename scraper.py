06b0a1122a68aca2bd4fcbd15b5725c2d827cf45
keelanfh <info@keelanfh.com>
2022-10-28 13:22:38 +0100

    Update to include queries

--- scraper.py
+++ scraper.py
@@ -12,8 +12,8 @@ class AssetSpider(scrapy.Spider):
     # custom_settings = {"CLOSESPIDER_PAGECOUNT": 5}
 
     def parse(self, response):
-
         yield {"url": response.url}
+
         # # Introspect all tags with an src or an href attribute for the target domain
         # for img in response.xpath('//@src|//@href').extract():
         #     if TARGET_DOMAIN in img:
@@ -24,14 +24,35 @@ class AssetSpider(scrapy.Spider):
 
         # Follow a href to find new pages
         for link in response.xpath("//a/@href").extract():
+            relative = False
             # We want to stay within the same site, consider a link to be relative if
             # it has a path but no host or scheme.
             # This excludes URIs like //www.example.com and mailto:example@example.com
             parsed = urlparse(link)
             relative = parsed.path and not parsed.netloc and not parsed.scheme
             # Follow the link if it's relative or if it's absolute within our domain
-            if FOLLOW_DOMAIN in link or relative:
-                if "?" in link:
-                    # Skip parameterised requests
+            if parsed.hostname:
+                if parsed.hostname in (FOLLOW_DOMAIN, f"www.{FOLLOW_DOMAIN}"):
+                    relative = True
+            if relative:
+                # if "?" in link:
+                #     # Skip parameterised requests
+                #     continue
+
+                if link.split(".")[-1] in (
+                    "mp4",
+                    "pdf",
+                    "docx",
+                    "doc",
+                    "xls",
+                    "xlsx",
+                    "ppt",
+                    "pptx",
+                ):
+                    # Skip non-HTML
+                    # These files will be repeated - need to remove these later
+                    yield {"url": link}
                     continue
                 yield response.follow(link, callback=self.parse)
+            else:
+                print(parsed)
