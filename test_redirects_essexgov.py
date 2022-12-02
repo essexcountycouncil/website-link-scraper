e3b30fa430b6c973ca4da83b59a8ab25f2176730
Keelan Fadden-Hopper <info@keelanfh.com>
2022-12-02 13:14:45 +0000

    Update for essex.gov.uk scraping

--- test_redirects_essexgov.py
+++ test_redirects_essexgov.py
@@ -1,46 +1,48 @@
 import pandas
 import requests
+import json
 
-urls = pandas.read_csv("output/output.csv")
-urls["status"] = 0
+with open("output/essexgov_redirs.json") as f:
+    redirects = json.load(f)
+
+urls = pandas.DataFrame()
+urls["original_url"] = ""
 urls["final_status"] = 0
 urls["final_location"] = ""
 
 try:
-    for idx, domain in urls.to_dict(orient="index").items():
-        url = domain["url"]
+    for idx, url in enumerate(redirects.keys()):
+        url = url.replace("*", "foo").replace("http://www.essex.gov.uk", "/").replace("https://www.essex.gov.uk", "/")
+        urls.at[idx, "original_url"] = url
+        url = "https://portal.whitemoss-5a7067b3.uksouth.azurecontainerapps.io" + url
+
+
         print(f"Checking {url}...")
 
         # First don't follow redirects
-        # Setting verify=False means it won't check validity of TLS certificates
-        # We need this because the site doesn't have a valid TLS certificate yet
-        # You should change this under most circumstances
-        original_response = requests.get(
-            url, allow_redirects=False, timeout=5, verify=False
-        )
-        # Update dataframe with the original status code
-        urls.at[idx, "status"] = original_response.status_code
+        # original_response = requests.get(
+        #     url, allow_redirects=False, timeout=5)
+        # # Update dataframe with the original status code
+        # urls.at[idx, "status"] = original_response.status_code
 
         # Now follow redirects
-        if 300 <= original_response.status_code < 400:
-            try:
-                # Setting verify=False means it won't check validity of TLS certificates
-                # You should change this under most circumstances
-                final_response = requests.get(url, timeout=5, verify=False)
-                urls.at[idx, "final_location"] = final_response.url
-            # record errors with code of 0
-            except (requests.exceptions.ConnectionError, requests.exceptions.SSLError):
-                urls.at[idx, "final_status"] = 0
-                continue
+        # if 300 <= original_response.status_code < 400:
+        try:
+            final_response = requests.get(url, timeout=5)
+            urls.at[idx, "final_location"] = final_response.url
+        # record errors with code of 0
+        except (requests.exceptions.ConnectionError, requests.exceptions.SSLError, requests.exceptions.ReadTimeout):
+            urls.at[idx, "final_status"] = 0
+            continue
 
-        else:
-            final_response = original_response
+        # else:
+        #     final_response = original_response
 
         urls.at[idx, "final_status"] = final_response.status_code
 
-# Export to an excel sheet if you interrupt the program
+# Export to an excel sheet if anything bad happens
 # So you don't always have to run the whole program
-except KeyboardInterrupt:
-    urls.to_excel("output/redirect_test_output.xlsx")
+except:
+    urls.to_excel("output/essexgov_redirect_test_output.xlsx")
 
-urls.to_excel("output/redirect_test_output.xlsx")
+urls.to_excel("output/essexgov_redirect_test_output.xlsx")
